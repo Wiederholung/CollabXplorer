1a5178457fd3fbdbf412246f8f1cd53ab5ec9142
wiederholung <wiederholung@outlook.com>
2022-09-06 16:06:29 +0800

    测试

--- back-end/app.py
+++ back-end/app.py
@@ -1,12 +1,21 @@
+import json
+
 from flask import Flask
+
+import getJSON
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
+@app.route('/getjson/<name_ch>')
+def get_json(name_ch):
+    return json.dumps(getJSON.get_data(name_ch=name_ch), ensure_ascii=False)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='::')
--- back-end/json说明.py
+++ back-end/json说明.py
@@ -1,14 +1,14 @@
 # 最后返回的json文件 nodes links categories
-fianaljson = {'nodes':[],'links':[],'categories':[]}
+fianaljson = {'nodes': [], 'links': [], 'categories': []}
 
 # 添加节点的字典格式,数据是瞎写的。id name symbolsize x y value category type
-#-200<=x,y<=+200
-addnodes = {"id": "0","name": "王俊翔","symbolSize": 60,"x": 0,"y": 0,"value": 108,"category": 0,"type": "node"}
+# -200<=x,y<=+200
+addnodes = {"id": "0", "name": "王俊翔", "symbolSize": 60, "x": 0, "y": 0, "value": 108, "category": 0, "type": "node"}
 # 将节点添加志json文件，每个字典是nodes列表的一部分
 fianaljson['nodes'].append(addnodes)
 
 # 添加连接的字典格式,数据是瞎写的。 source target type value
-addlinks = {"source": "0","target": "1","type": "link","value": 10}
+addlinks = {"source": "0", "target": "1", "type": "link", "value": 10}
 # 将节点添加志json文件，每个字典是links列表的一部分
 fianaljson['links'].append(addlinks)
 
@@ -18,9 +18,6 @@ addcategories = {"name": "国际学院"}
 fianaljson['categories'].append(addcategories)
 
 # 字典生成式，将两个列表打包成一个字典
-name=['id','name','x','y']
-data=['0','wjx',0,0]
-testnode=dict(zip(name,data))
-
-
-
+name = ['id', 'name', 'x', 'y']
+data = ['0', 'wjx', 0, 0]
+testnode = dict(zip(name, data))
