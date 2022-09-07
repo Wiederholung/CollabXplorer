e800c31721fb846cd497b55eda430e93377462cf
wiederholung <wiederholung@outlook.com>
2022-09-07 13:37:52 +0800

    1

--- back-end/getJSON.py
+++ back-end/getJSON.py
@@ -1,9 +1,8 @@
 import json
-import time
-
-from pymongo import MongoClient
 import math
 import random
+
+from pymongo import MongoClient
 
 client = MongoClient('mongodb://rwAll:ARS 5111-rwAll'
                      '@dev.metattri.com:27017')
@@ -11,12 +10,10 @@ db = client.bupt
 
 
 # 获取作者信息，只接受单一参数
-def get_auth_info(id, name_ch, name_en, category):
-
-    remember = None
-    if id is not None:
+def get_auth_info(id_s, name_ch, name_en, category):
+    if id_s is not None:
         for col in db.list_collection_names():
-            res = db[col].find_one({'id': id})
+            res = db[col].find_one({'id': id_s})
             if res is not None:
                 return res
         return None
@@ -38,29 +35,23 @@ def get_auth_info(id, name_ch, name_en, category):
         return None
 
 
-def get_data(id=None, name_ch=None, name_en=None, category=None):
+def get_data(id_s=None, name_ch=None, name_en=None, category=None):
     final_json = {'nodes': [], 'links': [], 'categories': []}
-    # nodes = ['id', 'name', 'symbolSize', 'x', 'y', 'value', 'category', 'type']
-    # links = ['source', 'target', 'value']
     # TODO: demo中categories只有三种，后续需要再添加学院
     categories = [{"name": "计算机学院"}, {"name": "数字媒体与设计艺术学院"}, {"name": "其他"}]
     index = None
-    var = 1
 
     # 添加固定的种类数至finalJson['categories']
     final_json['categories'].append(categories)
 
     # 获取中心作者个人信息
-    auth_info = get_auth_info(id, name_ch, name_en, category)
-    # print(auth_info)
+    auth_info = get_auth_info(id_s, name_ch, name_en, category)
     if auth_info is None:
         print('没有找到相关作者信息，请检查参数是否正确！')
         return None
 
     # 将中心作者添加至finalJson['nodes']
     final_json['nodes'].append(
-        # {'id': auth_info['id'], 'name': auth_info['name'], 'symbolSize': 30, 'x': 0, 'y': 0, 'value': 0,
-        #  'category': auth_info['category'], 'type': 'node'}
         {'id': auth_info['id'], 'name': auth_info['name'], 'symbolSize': 70, 'x': 0, 'y': 0, 'value': 0,
          'category': 0, 'type': 'node'}
     )
@@ -72,29 +63,22 @@ def get_data(id=None, name_ch=None, name_en=None, category=None):
             break
 
     # 遍历中心作者的所有合作者
-    while db[index].find_one({'相关老师' + str(var): {'$gte': "0"}}) is not None:
-        T1 = time.time()
-
-        # 获取合作信息
-        res = db[index].find({}, {'_id': 0, '相关老师' + str(var): 1, '相关文献:' + str(var): 1}).limit(1).skip(1)
-        # 获取合作者信息
-        co_auth_info = get_auth_info(None, None, res[0]['相关老师' + str(var)], None)
+    res = db[index].find({}, {'_id': 0, 'realted': 1}).skip(1).limit(1)[0]['realted']
+    for i in res:
+        co_auth_info = get_auth_info(None, None, i[0], None)
         # TODO: 目前，如果合作者不隶属BUPT，则跳过
         if co_auth_info is not None:
             # 添加合作者信息至finalJson['nodes']
-            # final_json['nodes'].append(
-            #     {'id': co_auth_info['id'], 'name': co_auth_info['name'], 'symbolSize': 60, 'x': 0, 'y': 0,
-            #      'value': int(res[0]['相关文献:' + str(var)]), 'category': co_auth_info['category'], 'type': 'node'})
             final_json['nodes'].append(
                 {'id': co_auth_info['id'], 'name': co_auth_info['name'], 'symbolSize': 60, 'x': 0, 'y': 0,
-                 'value': int(res[0]['相关文献:' + str(var)]), 'category': 0, 'type': 'node'})
+                 'value': int(i[1]), 'category': 0, 'type': 'node'})
             # 添加合作信息至finalJson['links']
             final_json['links'].append(
-                {'source': auth_info['id'], 'target': co_auth_info['id'], 'value': int(res[0]['相关文献:' + str(var)])})
-        var += 1
-        # print(res[0])
-        # 继续遍历下一段合作关系
+                {'source': auth_info['id'], 'target': co_auth_info['id'],
+                 "type": "link", 'value': int(i[1])}
+            )
 
+    # 计算合作者的坐标
     # 动态赋予x y值
     # 代码功能
 
@@ -136,24 +120,20 @@ def get_data(id=None, name_ch=None, name_en=None, category=None):
 
 
 if __name__ == '__main__':
-    # client = MongoClient('mongodb://rwAll:ARS 5111-rwAll'
-    #                      '@dev.metattri.com:27017')
-    # db = client.bupt
-    db['Anfu_Zhou'].createIndex({id: 1})
-    # # TODO: 目前finalJson['categories']为空
-    # # TODO: 判断是传入JSON文件 还是直接以JSON格式直接进行文本传递
-    #
-    # # 需要指定中心作者的id、name_ch、name_en中的一个，以下三种方式皆可
-    # finalJson = get_data(name_ch="丰雷")  # 演示：假设中心作者为“丰雷”老师
-    # # finalJson = get_data(id="101")
-    # # finalJson = get_data(name_en="Lei_Feng_0001")
-    #
-    # # 将finalJson字典转化为json，utf-8编码
+    # TODO: 目前finalJson['categories']为空
+    # TODO: 判断是传入JSON文件 还是直接以JSON格式直接进行文本传递
+
+    # 需要指定中心作者的id、name_ch、name_en中的一个，以下三种方式皆可
+    finalJson = get_data(name_ch="丰雷")  # 演示：假设中心作者为“丰雷”老师
+    # finalJson = get_data(id_s="101")
+    # finalJson = get_data(name_en="Lei_Feng_0001")
+
+    # 将finalJson字典转化为json，utf-8编码
     # dict_json = json.dumps(finalJson, ensure_ascii=False)
     # with open('file.json', 'w+', encoding="utf-8") as file:
     #     file.write(dict_json)
-    #
-    # print(finalJson['nodes'])
-    # print(len(finalJson['nodes']))
-    # print(finalJson['links'])
-    # print(len(finalJson['links']))
+
+    print(finalJson['nodes'])
+    print(len(finalJson['nodes']))
+    print(finalJson['links'])
+    print(len(finalJson['links']))
