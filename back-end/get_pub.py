bb81020e4975ba4a2881fcb2c0cfc4c3d7a701e4
wiederholung <wiederholung@outlook.com>
2022-09-27 19:39:10 +0800

    测试爬虫

--- back-end/get_pub.py
+++ back-end/get_pub.py
@@ -25,17 +25,6 @@ def get_dblp_pub(pid):
 
 
 def xml_browser(xml):
-    # 范例
-    # data1 = {
-    #     "id": 92,
-    #     "Article": {
-    #         "aid2": {"title": "学科推荐系统2", "author": {"王俊翔": "02/2100", "王伊哲": "02/2100"},
-    #                  "url": "https://doi.org/10.1109/COMPSAC54236.2022.00123", "year": 2100, "abstract": "xxxxxx"},
-    #         "aid1": {"title": "学科推荐系统1", "author": {"朱子炫": "02/2100", "胡逸同": "02/2100"},
-    #                  "url": "https://dblp.org/pid/65/9612.xml", "year": 2100, "abstract": "xxxxxx"}
-    #     }
-    # }
-
     # 获取根节点
     root = xml.documentElement
 
