889b5e95328b4f9f77e4fabd59cd7e4aa3be9f01
anonymity <1365882127@qq.com>
2022-11-01 19:10:52 +0800

    （补充注释，方便阅读

--- back-end/Algorithm/vec2value.py
+++ back-end/Algorithm/vec2value.py
@@ -1,14 +1,15 @@
+#利用维基百科将Word2vec训练完成后，就得到了语料库中的相应词的词向量，将评论信息转化为词向量的实质是利用“键值对”的原理，将特定词的词向量取出。将评论中的中文词全部转化为词向量后，整条评论的向量值就是其所有分词向量的平均值。核心代码如下。
+
 from gensim.models import Word2Vec
 import codecs
-import pymysql
 import numpy
 import Utils.articleUtils as articleUtils
 
 numpy.set_printoptions(suppress=True)
 
-fcoms = codecs.open('coms_all.txt', 'r', encoding="utf8")
-w2v_model = Word2Vec.load('wiki.zh.1.6gr.model')
-w = codecs.open('vec_all.txt', 'w', encoding="utf8")
+fcoms = codecs.open('coms_all.txt', 'r', encoding="utf8")  # 读取需要转化的内容
+w2v_model = Word2Vec.load('wiki.zh.1.6gr.model')  # 加载已经处理好的word2vec模型
+w = codecs.open('vec_all.txt', 'w', encoding="utf8")  # 写入结果
 size = 300  # 词向量维度
 
 
@@ -19,7 +20,7 @@ def count_vec_sentence():
     id = 0
     while line:
         words = articleUtils.stemmer(line)  # 通过stemmer分词
-        vec = numpy.zeros(size).reshape((1, size))
+        vec = numpy.zeros(size).reshape((1, size))  # 0矩阵
         vec0 = numpy.zeros(size).reshape((1, size))
         count = 0
         flag = True
@@ -31,7 +32,7 @@ def count_vec_sentence():
                     flag = False
                 else:
                     try:
-                        vec += w2v_model[word].reshape((1, size))
+                        vec += w2v_model[word].reshape((1, size))  # 词向量相加
                         count += 1
                     except KeyError:
                         # print('==== fault: ', word)
@@ -41,7 +42,7 @@ def count_vec_sentence():
         i += 1
         line = fcoms.readline()
         if count != 0:
-            vec /= count
+            vec /= count  # 计算每个文章词向量的平均值
             w.write(attitude + ' ')
             w.write(str(vec)[2:-2])
             w.write('$')
