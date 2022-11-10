23cc7c3f2197e74a7e9ac040ed1f6adb8c527a3d
wiederholung <wiederholung@outlook.com>
2022-11-10 10:57:48 +0800

    添加命令行运行事例

--- back-end/tests/wdir.py
+++ back-end/tests/wdir.py
@@ -1,5 +1,8 @@
 import os
 import sys
 
-os.chdir('../')  # change working directory to the root of the project
+os.chdir('/home/Projects/Academic-Collaboration-RS/back-end')  # change working directory to the root of the project
 sys.path.append(os.getcwd())  # add the root of the project to the path
+
+# 后台运行 py 脚本
+# nohup python -u name.py > log_name.log 2>&1 &
