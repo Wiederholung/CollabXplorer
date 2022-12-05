77788cb0270139dea3fcc5a3cbf2f0b9b194e25d
wiederholung <wiederholung@outlook.com>
2022-12-05 17:13:17 +0800

    使用 Selenium 驱动管理软件

--- back-end/utils/crawler/crawler_driver.py
+++ back-end/utils/crawler/crawler_driver.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 
 def init_driver(url):
-    driver = webdriver.Chrome()
     driver.implicitly_wait(30)  # 这是延时等待。由于网速时快时慢，
     driver.get(url)  # 而 get 方法会在网页框架加载结束后停止执行，这就会导致有些时候我们打算获取的内容还没被加载进来便结束了获取页面数据，最后报错，拿不到想要的数据。
     # 遇到class name中的复合情况（即有多个class name中间是空格隔开的）
